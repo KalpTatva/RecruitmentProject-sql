ad7a1888e6bfaf2e13dc077d620c782742e688d3
KalpTatva <tatva.pca117@outlook.com>
2025-09-08 10:50:29 +0530

    queries of job recruitment portal

--- RecruitmentPortalSchema.sql
+++ RecruitmentPortalSchema.sql
@@ -17,6 +17,11 @@ select * from CompanyLocations;
 select * from CompanyLocations_History;	
 select * from CompanySocialMedia;
 select * from CompanyStatus;
+select * from CompanyStatus_History;
+select * from JobCategory;
+
+select * from Jobs
+
 
 --------------
 -- update query
@@ -356,6 +361,106 @@ create table CompanyStatus_History (
 	DeletedById integer default 0,
 );
 
+
+-- jobs table
+create table Jobs (
+	JobId int identity(1,1) primary key,
+	
+	CompanyId int not null foreign key (CompanyId) references company(CompanyId),
+	CompanyLocationId int not null foreign key (CompanyLocationId) references CompanyLocations(CompanyLocationId),
+	JobCategoryId int not null foreign key (JobCategoryId) references JobCategory(JobCategoryId),
+
+	JobTitle varchar(255) not null,
+	JobType int not null,
+	JobDescription nvarchar(MAX),
+	JobRole varchar(255),
+	Experience varchar(200),
+	Degree varchar(200),
+	Tags nvarchar(Max),
+	
+	MinSalary numeric,
+	MaxSalary numeric,
+	
+	ApplicationStartDate datetime2,
+	ApllicationEndDate datetime2,
+
+	IsDeleted bit default 'false',
+	CreatedAt datetime2 default getdate(),
+	CreatedById integer default 0,
+	ModifiedAt datetime2, 
+	ModifiedById integer default 0,
+	DeletedAt datetime2,
+	DeletedById integer default 0,
+)
+
+
+
+
+
+-- jobs history table
+create table Jobs_History (
+	Job_HistoryId int identity(1,1) primary key,
+	
+	JobId int not null foreign key (JobId) references Jobs(JobId),
+	CompanyId int not null foreign key (CompanyId) references company(CompanyId),
+	CompanyLocationId int not null foreign key (CompanyLocationId) references CompanyLocations(CompanyLocationId),
+	JobCategoryId int not null foreign key (JobCategoryId) references JobCategory(JobCategoryId),
+
+	JobTitle varchar(255) not null,
+	JobType int not null,
+	JobDescription nvarchar(MAX),
+	JobRole varchar(255),
+	Experience varchar(200),
+	Degree varchar(200),
+	Tags nvarchar(Max),
+
+	MinSalary numeric,
+	MaxSalary numeric,
+	
+	ApplicationStartDate datetime2,
+	ApllicationEndDate datetime2,
+
+	IsDeleted bit default 'false',
+	CreatedAt datetime2 default getdate(),
+	CreatedById integer default 0,
+	ModifiedAt datetime2, 
+	ModifiedById integer default 0,
+	DeletedAt datetime2,
+	DeletedById integer default 0,
+)
+
+-- Job category table
+
+create table JobCategory (
+	JobCategoryId int identity(1,1) primary key,
+	CategoryName varchar(255),
+)
+
+-- job roles table
+
+create table JobRoles (
+	JobRoleId int identity(1,1) primary key,
+	JobRole varchar(500) not null
+)
+
+-- job types table
+
+create table JobTypes (
+	JobTypeId int identity(1,1) primary key,
+	JobType varchar(500) not null
+)
+
+-- degree table
+
+create table Degrees (
+	DegreeId int identity(1,1) primary key,
+	Degree varchar(500)
+)
+	
+
+
+
+
 --------------
 -- alter query
 --------------
@@ -421,8 +526,8 @@ create table CompanyStatus_History (
 --alter table CompanyLocations_History 
 --alter table Profile_History 
 
---drop column isDeleted, isModified, isCreated
---add operation CHAR(3) check(operation = 'INS' or operation = 'DEL' or operation = 'MOD') 
+--alter table jobs add DegreeId int not null foreign key (DegreeId) references Degrees(DegreeId)
+--alter table Jobs_History add Operation char(3)
 
 ---------------
 -- insert query
@@ -544,3 +649,183 @@ INSERT INTO City (StateId, CityName) VALUES
 INSERT INTO City (StateId, CityName) VALUES
 (15, 'Cologne'),
 (15, 'Düsseldorf');
+
+
+INSERT INTO JobCategory (CategoryName) VALUES
+('Information Technology (IT)'),
+('Software Development'),
+('Web Development'),
+('Mobile Development'),
+('Data Science & Analytics'),
+('Artificial Intelligence / Machine Learning'),
+('Cybersecurity'),
+('Cloud Computing & DevOps'),
+('Networking & System Administration'),
+('Project Management'),
+('Human Resources (HR)'),
+('Sales & Marketing'),
+('Customer Support / Service'),
+('Finance & Accounting'),
+('Administration & Office Support'),
+('Education & Training'),
+('Healthcare & Medical'),
+('Engineering'),
+('Design & Creative Arts'),
+('Legal & Compliance');
+
+
+insert into JobTypes (JobType) values 
+('Full-time'),
+('Part-time'),
+('Self-employeed'),
+('FreeLance'),
+('Internship'),
+('Trainee')
+
+
+insert into JobRoles (JobRole) value
+('Junior Software Developer'),
+('Junior DevOps Engineer'),
+('Software Engineer'),
+('Data Scientist'),
+('Database Administrator'),
+('Web Developer'),
+('Software Principal Engineer'),
+('Lead Developer'),
+('Director of Engineering'),
+('Data Analyst'),
+('Research Assistant'),
+('Data Analyst (Mid)'),
+('Data Analytics Specialist'),
+('Research Analyst'),
+('Chief Data Officer'),
+('Director of Analytics'),
+('Computer Support Specialist'),
+('IT Software Developer'),
+('IT Specialist'),
+('Network Administrator'),
+('Cloud Engineer'),
+('Director of IT'),
+('IT Manager'),
+('Information Security Analyst'),
+('Chief Information Security Officer (CISO)'),
+('Medical Assistant'),
+('Phlebotomist'),
+('Nursing Assistant'),
+('Registered Nurse (RN)'),
+('Pharmacist'),
+('Respiratory Therapist'),
+('Physician'),
+('Surgeon'),
+('Nurse Anesthetist'),
+('Medical Transcriptionist'),
+('Health Information Technician'),
+('Health Inspector'),
+('Office Manager'),
+('Hospital Administrator'),
+('Director of Healthcare Services'),
+('Physical Therapist'),
+('Diagnostic Medical Sonographer'),
+('Hematology Consultant'),
+('Bank Teller'),
+('Credit Analyst'),
+('Financial Analyst'),
+('Loan Officer'),
+('Portfolio Manager'),
+('Investment Banker'),
+('Financial Manager'),
+('Chief Financial Officer (CFO)'),
+('Accounts Payable Clerk'),
+('MIS Executive'),
+('Accountant'),
+('Auditor'),
+('Chartered Accountant (CA)'),
+('Insurance Sales Agent'),
+('Insurance Underwriter'),
+('Actuary'),
+('Marketing Coordinator'),
+('Social Media Coordinator'),
+('Social Media Manager'),
+('Content Marketing Manager'),
+('SEO Specialist'),
+('Marketing Director'),
+('General Manager - Digital Marketing'),
+('Chief Marketing Officer (CMO)'),
+('Sales Representative'),
+('Sales Associate'),
+('Retail Sales Executive'),
+('Sales Manager'),
+('Account Executive'),
+('Sales Consultant'),
+('Regional Sales Manager'),
+('Sales Director'),
+('Laborer'),
+('Surveyor'),
+('Billing Engineer'),
+('Assistant Construction Manager'),
+('Construction Manager'),
+('Director of Construction'),
+('Real Estate Consultant'),
+('Property Manager'),
+('Real Estate Broker'),
+('Commercial Real Estate Specialist'),
+('Production Operator'),
+('Process Technician'),
+('Warehouse Supervisor'),
+('Quality Assurance Specialist'),
+('Vendor Coordinator'),
+('Operations Manager'),
+('Lead Role (Greenfield Project)'),
+('Logistics Coordinator'),
+('Supply Chain Specialist'),
+('HR Assistant'),
+('Recruiter'),
+('HR Manager'),
+('Employee Relations Officer'),
+('Head of People & Culture'),
+('Chief Human Resources Officer (CHRO)'),
+('Reservations Agent'),
+('Front Desk Coordinator'),
+('Event Manager'),
+('Restaurant Manager'),
+('Hotel General Manager'),
+('Regional Manager'),
+('Concierge'),
+('Housekeeper'),
+('Waiter'),
+('Executive Chef'),
+('Tour Guide'),
+('Teacher'),
+('Lecturer'),
+('Librarian'),
+('Principal'),
+('Professor'),
+('Centre Head'),
+('Education Counsellor'),
+('Human Resource Manager (Education Institute)'),
+('Telecommunications Technician'),
+('Data Analyst (Telecom)'),
+('Telecom Engineer'),
+('Senior Telecom Engineer'),
+('Director of Telecom Operations'),
+('Project Coordinator'),
+('Call Center Manager'),
+('Senior Project Manager'),
+('Administrative Assistant'),
+('Receptionist'),
+('Office Manager'),
+('Executive Assistant'),
+('Chief of Operations (COO)'),
+('Managing Director (MD)'),
+('Director'),
+('Executive Director'),
+('Vice President (VP)'),
+('Team Leader'),
+('Chairman'),
+('Chief Executive Officer (CEO)');
+go
+
+
+insert into Degrees (Degree) values 
+('Associate'), ('Bachelor'), ('Master'), ('PHD');
+go
--- RecruitmentPortalSchemaTriggers.sql
+++ RecruitmentPortalSchemaTriggers.sql
@@ -4,19 +4,21 @@ use RecruitmentPortal;
 -- triggers 
 -----------------------
 select * from Users;
-select * from Users_History;
 select * from profile;
+select * from Users_History;
 select * from Profile_History;
-select * from Company;
 select * from Company_History;
+select * from CompanyStatus_History;
+select * from CompanyLocations_History;
+select * from Company;
 select * from City;
 select * from Country;
 select * from State;
 select * from CompanyLocations;
-select * from CompanyLocations_History;
 select * from CompanySocialMedia;
 select * from CompanyStatus;
-select * from CompanyStatus_History;
+select * from Jobs_History;
+select * from Jobs;
 go
 
 -- user history insert / delete triggers
@@ -69,27 +71,36 @@ after update
 as 
 begin
 	set nocount on;
-	insert into Users_History (
-			UserId,
-			UserName,
-			Email,
-			Role,
-			operation,
-			ModifiedAt,
-			ModifiedById
-		)
-		-- update 
-		select 
-			i.UserId,
-			i.UserName,
-			i.Email,
-			i.Role,
-			'MOD',
-			i.ModifiedAt,
-			i.ModifiedById
-		from inserted as i;
+
+	declare @oldUserName varchar(255); 
+	declare @newUserName varchar(255);
+	select @oldUserName = d.UserName from deleted d;
+	select @newUserName = i.UserName from inserted i;
+
+	declare @oldEmail varchar(255); 
+	declare @newEmail varchar(255);
+	select @oldEmail = d.Email from deleted d;
+	select @newEmail = i.Email from inserted i;
+
+	declare @oldRole varchar(500); 
+	declare @newRole varchar(500);
+	select @oldRole = d.Role from deleted d;
+	select @newRole = i.Role from inserted i;
+
+	if @oldUserName <> @newUserName or
+	@oldEmail <> @newEmail or
+	@oldRole <> @newRole 
+	begin
+		insert into Users_History (
+				UserId, UserName, Email, Role, operation, ModifiedAt, ModifiedById
+			)
+			-- update 
+		select d.UserId, d.UserName, d.Email, d.Role, 'MOD', i.ModifiedAt, i.ModifiedById
+		from deleted d join inserted as i on i.UserId = d.UserId;
+	end;
 end;
 go
+
 
 
 -- profile history insert / delete triggers
@@ -166,7 +177,7 @@ begin
 			d.CreatedById,
 			d.ModifiedAt,
 			d.ModifiedById,
-			getdate(),
+			d.DeletedAt,
 			d.DeletedById,
 			'DEL'
 		from deleted d;
@@ -179,54 +190,100 @@ after update
 as 
 begin	
 	set nocount on;
-	insert into Profile_History (
-		ProfileId,
-		FirstName,
-		MiddleName,
-		LastName,
-		Gender,
-		ImageURl,
-		DateOfBirth,
-		Address,
-		Phone,
-		pincode,
-		CountryId,
-		StateId,
-		CityId,
-		CountryCode,
-		CreatedAt,
-		CreatedById,
-		ModifiedAt,
-		ModifiedById,
-		DeletedAt,
-		DeletedById,
-		operation)
-		-- update 
-		select
-			i.ProfileId,
-			i.FirstName,
-			i.MiddleName,
-			i.LastName,
-			i.Gender,
-			i.ImageURl,
-			i.DateOfBirth,
-			i.Address,
-			i.Phone,
-			i.pincode,
-			i.CountryId,
-			i.StateId,
-			i.CityId,
-			i.CountryCode,
-			i.CreatedAt,
-			i.CreatedById,
-			getdate(),
-			i.ModifiedById,
-			i.DeletedAt,
-			i.DeletedById,
-			'MOD'
-		from inserted i
+	declare @oldFirstName varchar(255); 
+	declare @newFirstName varchar(255);
+	select @oldFirstName = d.FirstName from deleted d;
+	select @newFirstName = i.FirstName from inserted i;
+
+	declare @oldMiddleName varchar(255); 
+	declare @newMiddleName varchar(255);
+	select @oldMiddleName = d.MiddleName from deleted d;
+	select @newMiddleName = i.MiddleName from inserted i;
+
+	declare @oldLastName varchar(500); 
+	declare @newLastName varchar(500);
+	select @oldLastName = d.LastName from deleted d;
+	select @newLastName = i.LastName from inserted i;
+
+	declare @oldGender varchar(Max); 
+	declare @newGender varchar(Max);
+	select @oldGender = d.Gender from deleted d;
+	select @newGender = i.Gender from inserted i;
+
+	declare @oldImageURl varchar(500); 
+	declare @newImageURl varchar(500);
+	select @oldImageURl = d.ImageURl from deleted d;
+	select @newImageURl = i.ImageURl from inserted i;
+
+	declare @oldDateOfBirth varchar(255); 
+	declare @newDateOfBirth varchar(255);
+	select @oldDateOfBirth = d.DateOfBirth from deleted d;
+	select @newDateOfBirth = i.DateOfBirth from inserted i;
+
+	declare @oldPhone BIGINT; 
+	declare @newPhone BIGINT;
+	select @oldPhone = d.Phone from deleted d;
+	select @newPhone = i.Phone from inserted i;
+	
+	declare @oldpincode BIGINT; 
+	declare @newpincode BIGINT;
+	select @oldpincode = d.pincode from deleted d;
+	select @newpincode = i.pincode from inserted i;
+
+	declare @oldAddress varchar(500);
+	declare @newAddress varchar(500);
+	select @oldAddress = d.Address from deleted d;
+	select @newAddress = i.Address from inserted i;
+
+	declare @oldCountryId int;
+	declare @newCountryId int;
+	select @oldCountryId = d.CountryId from deleted d;
+	select @newCountryId = i.CountryId from inserted i;
+
+	declare @oldStateId int;
+	declare @newStateId int;
+	select @oldStateId = d.StateId from deleted d;
+	select @newStateId = i.StateId from inserted i;
+
+	declare @newcityId int;
+	declare @oldcityId int;
+	select @oldcityId = d.cityId from deleted d;
+	select @newcityId = i.cityId from inserted i;
+
+	declare @oldCountryCode varchar(10); 
+	declare @newCountryCode varchar(10);
+	select @oldCountryCode = d.CountryCode from deleted d;
+	select @newCountryCode = i.CountryCode from inserted i;
+
+
+
+	if @oldFirstName <> @newFirstName or
+	@oldMiddleName <> @newMiddleName or
+	@oldLastName <> @newLastName or
+	@oldGender <> @newGender or
+	@oldImageURl <> @newImageURl or
+	@oldDateOfBirth <> @newDateOfBirth or
+	@oldAddress <> @newAddress or
+	@oldPhone <> @newPhone or
+	@oldpincode <> @newpincode or
+	@oldCountryCode <> @newCountryCode or
+	@oldCountryId <> @newCountryId or 
+	@oldStateId <> @newStateId or 
+	@oldcityId <> @newcityId 
+	begin
+		insert into Profile_History (
+			ProfileId, FirstName, MiddleName, LastName, Gender, ImageURl, DateOfBirth, Address, Phone, pincode, CountryId, StateId, CityId, CountryCode,
+			CreatedAt, CreatedById, ModifiedAt, ModifiedById, DeletedAt, DeletedById, operation)
+			-- update 
+			select
+				d.ProfileId, @oldFirstName, @oldMiddleName, @oldLastName, @oldGender, @oldImageURl, @oldDateOfBirth, @oldAddress, @oldPhone, @oldpincode, @oldCountryId,
+				@oldStateId, @oldcityId, @oldCountryCode, null, 0, i.ModifiedAt, i.ModifiedById, null, 0, 'MOD'
+		from deleted d
+			join inserted i on i.ProfileId = d.ProfileId
+	end;
 end;
 go
+
 
 
 -- company history insert / delete triggers
@@ -249,7 +306,7 @@ begin
 	-- delete
 	select
 		d.CompanyId, d.UserId, d.CompanyName, d.CompanyType, d.CompanyWebsite, d.Description, d.Location, d.CountryCode, d.Phone, d.ImageURl, null , 0,
-		null , 0, getdate(), 0, 'DEL'
+		null , 0, d.DeletedAt, d.DeletedById, 'DEL'
 	from deleted d 
 end;
 go
@@ -260,17 +317,69 @@ after update
 as 
 begin	
 	set nocount on;
-	insert into company_History (
-		CompanyId, UserId, CompanyName, CompanyType, CompanyWebsite, Description, Location, CountryCode, Phone, ImageURl, CreatedAt, CreatedById, ModifiedAt, ModifiedById, 
-		DeletedAt, DeletedById, operation
-	)
-	-- insert 
-	select
-		i.CompanyId, i.UserId, i.CompanyName, i.CompanyType, i.CompanyWebsite, i.Description, i.Location, i.CountryCode, i.Phone, i.ImageURl, null, 0,
-		i.ModifiedAt, i.ModifiedById, null, 0, 'MOD'
-	from inserted i 
+	declare @oldCompanyName varchar(255); 
+	declare @newCompanyName varchar(255);
+	select @oldCompanyName = d.CompanyName from deleted d;
+	select @newCompanyName = i.CompanyName from inserted i;
+
+	declare @oldCompanyType varchar(255); 
+	declare @newCompanyType varchar(255);
+	select @oldCompanyType = d.CompanyType from deleted d;
+	select @newCompanyType = i.CompanyType from inserted i;
+
+	declare @oldCompanyWebsite varchar(500); 
+	declare @newCompanyWebsite varchar(500);
+	select @oldCompanyWebsite = d.CompanyWebsite from deleted d;
+	select @newCompanyWebsite = i.CompanyWebsite from inserted i;
+
+	declare @oldDescription varchar(Max); 
+	declare @newDescription varchar(Max);
+	select @oldDescription = d.Description from deleted d;
+	select @newDescription = i.Description from inserted i;
+
+	declare @oldLocation varchar(255); 
+	declare @newLocation varchar(255);
+	select @oldLocation = d.Location from deleted d;
+	select @newLocation = i.Location from inserted i;
+
+	declare @oldCountryCode varchar(10); 
+	declare @newCountryCode varchar(10);
+	select @oldCountryCode = d.CountryCode from deleted d;
+	select @newCountryCode = i.CountryCode from inserted i;
+
+	declare @oldPhone BIGINT; 
+	declare @newPhone BIGINT;
+	select @oldPhone = d.Phone from deleted d;
+	select @newPhone = i.Phone from inserted i;
+
+	declare @oldImageURl varchar(500); 
+	declare @newImageURl varchar(500);
+	select @oldImageURl = d.ImageURl from deleted d;
+	select @newImageURl = i.ImageURl from inserted i;
+
+	if @oldCompanyName <> @newCompanyName or
+	@oldCompanyType <> @newCompanyType or
+	@oldCompanyWebsite <> @newCompanyWebsite or
+	@oldDescription <> @newDescription or
+	@oldLocation <> @newLocation or
+	@oldCountryCode <> @newCountryCode or
+	@oldPhone <> @newPhone or
+	@oldImageURl <> @newImageURl 
+	begin
+		insert into company_History (
+			CompanyId, UserId, CompanyName, CompanyType, CompanyWebsite, Description, Location, CountryCode, Phone, ImageURl, CreatedAt, CreatedById, ModifiedAt, ModifiedById, 
+			DeletedAt, DeletedById, operation
+		)
+		-- insert 
+		select
+			d.CompanyId, d.UserId, @oldCompanyName, @oldCompanyType, @oldCompanyWebsite, @oldDescription, @oldLocation, @oldCountryCode, @oldPhone, @oldImageURl, null, 0,
+			i.ModifiedAt, i.ModifiedById, null, 0, 'MOD'
+		from deleted d 
+		join inserted i on d.CompanyId = i.CompanyId
+	end;
 end;
 go
+
 
 
 -- companyLocation history insert/delete triggers
@@ -286,7 +395,7 @@ begin
 	) 
 	select i.CompanyLocationId, i.CompanyId, i.CountryId, i.StateId, i.CityId, i.Address, i.CreatedAt, i.CreatedById, null, 0, null, 0, 'INS' from inserted i
 	union all 
-	select d.CompanyLocationId, d.CompanyId, d.CountryId, d.StateId, d.CityId, d.Address, null, 0, null, 0, getdate(), d.DeletedById, 'DEL' from deleted d
+	select d.CompanyLocationId, d.CompanyId, d.CountryId, d.StateId, d.CityId, d.Address, null, 0, null, 0, d.DeletedAt, d.DeletedById, 'DEL' from deleted d
 end;
 go
 -- companyLocation history update triggers
@@ -296,10 +405,38 @@ after update
 as
 begin
 	set nocount on;
+	
+	-- declaretion of variables 
+	declare @oldCountryId int;
+	declare @newCountryId int;
+	declare @oldStateId int;
+	declare @newStateId int;
+	declare @oldcityId int;
+	declare @newcityId int;
+	declare @oldAddress varchar(500);
+	declare @newAddress varchar(500);
+
+	select @oldCountryId = d.CountryId from deleted d;
+	select @newCountryId = i.CountryId from inserted i;
+	select @oldStateId = d.StateId from deleted d;
+	select @newStateId = i.StateId from inserted i;
+	select @oldcityId = d.cityId from deleted d;
+	select @newcityId = i.cityId from inserted i;
+	select @oldAddress = d.Address from deleted d;
+	select @newAddress = i.Address from inserted i;
+	 
+	if @oldCountryId <> @newCountryId or 
+	   @oldStateId <> @newStateId or 
+	   @oldcityId <> @newcityId or 
+	   @oldAddress <> @newAddress
+	begin
 	insert into CompanyLocations_History(
 		CompanyLocationId, CompanyId, CountryId, StateId, cityId, Address, CreatedAt, CreatedById, ModifiedAt, ModifiedById, DeletedAt, DeletedById, operation
 	) 
-	select i.CompanyLocationId, i.CompanyId, i.CountryId, i.StateId, i.CityId, i.Address, null, 0, getdate(), i.ModifiedById, null, 0, 'MOD' from inserted i
+	select d.CompanyLocationId, d.CompanyId,  @oldCountryId, @oldStateId, @oldcityId,  @oldAddress, null, 0, i.ModifiedAt, i.ModifiedById, null, 0, 'MOD' 
+	from deleted d
+	join inserted i on i.CompanyLocationId = d.CompanyLocationId
+	end;
 end;
 go
 
@@ -322,25 +459,107 @@ begin
 	union all 
 	select 
 		d.CompanyStatusId, d.CompanyId, d.CompanyFoundedYear, d.IndustryType, d.NumberOfFounders, d.TotalEmployees, d.TotalMaleEmployees, d.TotalFemaleEmployees,
-		d.TotalOtherEmployees, d.TotalRevenue, null, 0, null, 0, getdate(), d.DeletedById, 'DEL' 
+		d.TotalOtherEmployees, d.TotalRevenue, null, 0, null, 0, d.DeletedAt, d.DeletedById, 'DEL' 
 	from deleted d
 end;
 go
-
 -- companyStatus history update triggers
 create or alter trigger trg_companyStatus_history_ins_mod
 on CompanyStatus
 after update
 as
 begin
-	insert into CompanyStatus_history (
-		CompanyStatusId, CompanyId, CompanyFoundedYear, IndustryType, NumberOfFounders, TotalEmployees, TotalMaleEmployees, TotalFemaleEmployees,
-		TotalOtherEmployees, TotalRevenue, CreatedAt, CreatedById, ModifiedAt, ModifiedById, DeletedAt, DeletedById, operation
-	)
-	select 
-		i.CompanyStatusId, i.CompanyId, i.CompanyFoundedYear, i.IndustryType, i.NumberOfFounders, i.TotalEmployees, i.TotalMaleEmployees, i.TotalFemaleEmployees,
-		i.TotalOtherEmployees, i.TotalRevenue,  null, 0, getdate(), i.ModifiedById, null, 0, 'MOD' 
-	from inserted i 
+	set nocount on;
+	
+	declare @oldCompanyFoundedYear int; 
+	declare @newCompanyFoundedYear int;
+	select @oldCompanyFoundedYear = d.CompanyFoundedYear from deleted d;
+	select @newCompanyFoundedYear = i.CompanyFoundedYear from inserted i;
+
+	declare @oldIndustryType varchar(100);
+	declare @newIndustryType varchar(100);
+	select @oldIndustryType = d.IndustryType from deleted d;
+	select @newIndustryType = i.IndustryType from inserted i;
+
+	declare @oldNumberOfFounders int;
+	declare @newNumberOfFounders int;
+	select @oldNumberOfFounders = d.NumberOfFounders from deleted d;
+	select @newNumberOfFounders = i.NumberOfFounders from inserted i;
+
+	declare @oldTotalEmployees int;
+	declare @newTotalEmployees int;
+	select @oldTotalEmployees = d.TotalEmployees from deleted d;
+	select @newTotalEmployees = i.TotalEmployees from inserted i;
+
+	declare @oldTotalMaleEmployees int;
+	declare @newTotalMaleEmployees int;
+	select @oldTotalMaleEmployees = d.TotalMaleEmployees from deleted d;
+	select @newTotalMaleEmployees = i.TotalMaleEmployees from inserted i;
+
+	declare @oldTotalFemaleEmployees int;
+	declare @newTotalFemaleEmployees int;
+	select @oldTotalFemaleEmployees = d.TotalFemaleEmployees from deleted d;
+	select @newTotalFemaleEmployees = i.TotalFemaleEmployees from inserted i;
+
+	declare @oldTotalOtherEmployees int;
+	declare @newTotalOtherEmployees int;
+	select @oldTotalOtherEmployees = d.TotalOtherEmployees from deleted d;
+	select @newTotalOtherEmployees = i.TotalOtherEmployees from inserted i;
+
+	declare @oldTotalRevenue numeric;
+	declare @newTotalRevenue numeric;
+	select @oldTotalRevenue = d.TotalRevenue from deleted d;
+	select @newTotalRevenue = i.TotalRevenue from inserted i;
+
+	if @oldCompanyFoundedYear <> @newCompanyFoundedYear or 
+	   @oldIndustryType <> @newIndustryType or 
+	   @oldNumberOfFounders <> @newNumberOfFounders or 
+	   @oldTotalEmployees <> @newTotalEmployees or 
+	   @oldTotalMaleEmployees <> @newTotalMaleEmployees or 
+	   @oldTotalFemaleEmployees <> @newTotalFemaleEmployees or 
+	   @oldTotalOtherEmployees <> @newTotalOtherEmployees or 
+	   @oldTotalRevenue <> @newTotalRevenue
+	begin
+		insert into CompanyStatus_history (
+			CompanyStatusId, CompanyId, CompanyFoundedYear, IndustryType, NumberOfFounders, TotalEmployees, TotalMaleEmployees, TotalFemaleEmployees,
+			TotalOtherEmployees, TotalRevenue, CreatedAt, CreatedById, ModifiedAt, ModifiedById, DeletedAt, DeletedById, operation
+		)
+		select 
+			d.CompanyStatusId, d.CompanyId, @oldCompanyFoundedYear, @oldIndustryType, @oldNumberOfFounders, @oldTotalEmployees, @oldTotalMaleEmployees, @oldTotalFemaleEmployees,
+			@oldTotalOtherEmployees, @oldTotalRevenue,  null, 0, i.ModifiedAt, i.ModifiedById, null, 0, 'MOD' 
+		from deleted d 
+		join inserted i on i.CompanyStatusId = d.CompanyStatusId
+	end;
 end;
 go
 
+
+-- jobs insert/delete trigger
+
+create or alter trigger trg_Jobs_history_ins_del 
+on Jobs
+after insert, delete
+as
+begin
+	insert into Jobs_History (
+		JobId, CompanyId, CompanyLocationId, JobCategoryId, JobTitle, JobTypeId, JobDescription, JobRoleId, Experience, DegreeId, Tags, MinSalary,
+	    MaxSalary, ApplicationStartDate, ApllicationEndDate, IsDeleted, CreatedAt, CreatedById, ModifiedAt, ModifiedById, DeletedAt, DeletedById, Operation
+	)
+	select 
+		 i.JobId, i.CompanyId, i.CompanyLocationId, i.JobCategoryId, i.JobTitle, i.JobTypeId, i.JobDescription, 
+		 i.JobRoleId, i.Experience, i.DegreeId, i.Tags, i.MinSalary,
+	     i.MaxSalary, i.ApplicationStartDate, i.ApllicationEndDate, 
+		 i.IsDeleted, i.CreatedAt, i.CreatedById, i.ModifiedAt, i.ModifiedById , i.DeletedAt, i.DeletedById, 'INS'
+	from inserted i 
+union all 
+	select 
+		d.JobId, d.CompanyId, d.CompanyLocationId, d.JobCategoryId, d.JobTitle, d.JobTypeId, d.JobDescription,
+		d.JobRoleId, d.Experience, d.DegreeId, d.Tags, d.MinSalary,
+		d.MaxSalary, d.ApplicationStartDate, d.ApllicationEndDate, 
+		d.IsDeleted, d.CreatedAt, d.CreatedById, d.ModifiedAt, d.ModifiedById , d.DeletedAt, d.DeletedById, 'DEL'
+		from deleted d 
+		
+end;
+go
+
+select * from Jobs_History
